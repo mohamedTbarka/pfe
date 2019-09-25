9266d51118de9719e16952f84675cf54474e0d67
wbc <abdelaaziz.sisti@smarteez.eu>
2019-09-25 17:16:11 +0100

    add marque details

--- app/marque/urls.py
+++ app/marque/urls.py
@@ -4,5 +4,6 @@ from marque import views
 
 urlpatterns = [
     path('marques/', views.MarqueListView.as_view(), name='marques'),
+    path('marque/<int:pk>/', views.MarqueDetailView.as_view(), name='marque_detail'),
 
 ]
