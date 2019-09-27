de467f5ae25b67a12e80399f14dd0042bae42a8c
wbc <abdelaaziz.sisti@smarteez.eu>
2019-09-27 16:07:00 +0100

    fix contact api

--- app/contact/serializers.py
+++ app/contact/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 
-from contact.models import Newsletter
+from contact.models import Newsletter, Contact
 
 
 class NewsletterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Newsletter
+        fields = ('email',)
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
         fields = '__all__'
