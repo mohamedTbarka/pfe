f18efa3759eeb8280c4288acf230666977a798ce
wbc <abdelaaziz.sisti@smarteez.eu>
2019-09-27 15:02:57 +0100

    fix search

--- app/marque/search_indexes.py
+++ app/marque/search_indexes.py
@@ -6,12 +6,9 @@ from marque.models import Marque
 
 class NoteIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    title = indexes.CharField(model_attr='name')
-    body = indexes.CharField(model_attr='content')
+    name = indexes.CharField(model_attr='name')
+    content = indexes.CharField(model_attr='content')
 
     def get_model(self):
         return Marque
 
-    def index_queryset(self, using=None):
-        """Used when the entire index for model is updated."""
-        return self.get_model().objects.all()
