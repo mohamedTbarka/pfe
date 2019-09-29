1fbd14e0abb88ee984c4c4a5c78c2f0394bac5ef
mohamed <simo.tbarka@gmail.com>
2019-09-29 17:20:59 +0100

    the report should be reviewed and the presentation is started up !

---
+++ Documents/perl.pl
@@ -0,0 +1,5 @@
+$v = 1;
+while($v<=10) {
+	print("this is the $v iteration !\n");
+	$v = $v + 1
+}
---
+++ Documents/python.py
@@ -0,0 +1,4 @@
+i = 1
+while i <= 10 :
+    print "this is the " + str(i) + " iteration !"
+    i=i+1
