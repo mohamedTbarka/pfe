5a908b04371adbe948608312b8b41031351e2452
nassim <nassim.seffar@smarteez.eu>
2020-02-07 17:30:58 +0100

    sv project

--- app/static/js/valentine.js
+++ app/static/js/valentine.js
@@ -15,63 +15,43 @@ $(document).ready(function($) {
     
   }
   document.getElementById("uploadlogo").addEventListener("change", readFile);
-    // start statement
-    // Initialize form validation on the registration form.
-    // It has the name attribute "registration"
-    
-    $("form[name='registration']").validate({
-      // Specify validation rules
-      rules: {
-        // The key name on the left side is the name attribute
-        // of an input field. Validation rules are defined
-        // on the right side
-        image: {
-          required: true
-        },
-        fullname: "required",
-        email: {
-          required: true,
-          // Specify that email should be validated
-          // by the built-in "email" rule
-          email: true
-        },
-        phone: {
-          required: true
-        }
-      },
-      // Specify validation error messages
-      messages: {
-        fullname: "Veuillez remplir ce champ",
-        email: "Veuillez remplir ce champ",
-        phone: "Veuillez remplir ce champ",
-        image: "charger une image"
-      },
-      // Make sure the form is submitted to the destination defined
-      // in the "action" attribute of the form when valid
-      submitHandler: function(form) {
-        form.submit();
-        
-      }
-    });
+
     
     // end statement
     var current_url = document.location.href;
-    $(".form").submit(function(e) {
+    $('form[name="registration"').submit(function(e) {
       e.preventDefault();
-    });
-    $('a.start').on('click', function(){
-      $('.content-one').slideUp();
-      $('.content-one').addClass('hide');
-      $('.content-one').removeClass('show');
-      $('.content-two').slideDown();
-      $('.content-two').removeClass('hide');
-      $('.content-two').addClass('show');
-    })
-    $('a.to-step-3').on('click', function(){
       var fullname = $('input[name="fullname"').val();
       var email = $('input[name="email"').val();
       var phone = $('input[name="phone"').val();
-      var img = $('#uploadlogo').val();
+      var img = $('#ticker_value').val();
+      $.ajax({
+        method: "POST",
+        url: "/concours-saintvalentin/sinscrire/",
+        data: {
+          'full_name': fullname,
+          'email': email,
+          'telephone':phone,
+          'ticket_base64': img.split("base64,")[1],
+        },
+        statusCode: {
+          406: function(error){
+            console.log('Attendez 24H pour reparticiper', error);
+          },
+          200: function(response){
+            console.log('Inscription correcte');
+            localStorage.setItem('quiz',JSON.stringify(response));
+            window.location = '/concours-saintvalentin/quiz/';
+          },
+          500: function(message){
+            console.log('Erreur 500', message.responseText);
+          }
+        }
+      });
+    });
+    
+    $('a.to-step-3').on('click', function(){
+      
 
       // $.ajax({
       //   url: "sinscrire/",
@@ -98,13 +78,6 @@ $(document).ready(function($) {
       $('.content-four').slideDown();
       $('.content-four').removeClass('hide');
       $('.content-four').addClass('show');
-    })
-    // Radio Color Background
-    $('.choice').on('click', function(){
-      if($(this).find('input[type="radio"]').is(':checked')){
-        $('.choice').removeClass('checked');
-        $(this).addClass('checked');
-      }
     })
   
     // Upload btn on change call function
