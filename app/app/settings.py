375b86ef4551b618777ea7d2ac80ec121e2e2138
nassim <nassim.seffar@smarteez.eu>
2020-02-10 18:03:04 +0100

    local settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -186,10 +186,7 @@ else:
     ALLOWED_HOSTS = json.loads(os.environ.get('ALLOWED_HOSTS', '[]'))
     ADMINS = json.loads(os.environ.get('ADMINS', '[]'))
     MANAGERS = ADMINS
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-DEBUG = True
+try:
+    from .local_settings import *
+except ImportError:
+    pass
